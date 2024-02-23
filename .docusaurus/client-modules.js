870030a7ee19a54b87e59e017e1efc8528332003
André Almeida Oliveira <andreaoliveira@ua.pt>
2024-02-23 17:30:52 +0000

    delete useless things and add gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/andreoliveira/Documents/GitHub/microsite/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/andreoliveira/Documents/GitHub/microsite/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/andreoliveira/Documents/GitHub/microsite/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/andreoliveira/Documents/GitHub/microsite/src/css/custom.css"),
-];
